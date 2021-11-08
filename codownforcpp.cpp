2f182d37240a49fd25bd897f1ce54c1591149182
Rohan Kasabe <71020327+ROHAN0011@users.noreply.github.com>
2021-11-08 17:36:35 +0530

    Update codownforcpp.cpp

--- codownforcpp.cpp
+++ codownforcpp.cpp
@@ -2746,3 +2746,141 @@ int main()
 
     return 0;
 }
+
+
+
+//61. Operations using Class.
+
+#include <iostream>
+using namespace std;
+
+//define class
+class operations
+{
+    //member variables
+    public:
+        int num1,num2;
+
+    //member function or methods
+    public:
+       void add()
+       {
+           cout<<"enter two number for addition : ";
+           cin>>num1>>num2;
+           cout<<"addition = "<<num1+num2;
+           cout<<"\n";
+       }
+
+        void sub()
+       {
+           cout<<"enter two number for subtraction : ";
+           cin>>num1>>num2;
+           cout<<"addition = "<<num1-num2;
+           cout<<"\n";
+       }
+
+        void mul()
+       {
+           cout<<"enter two number for multiplication : ";
+           cin>>num1>>num2;
+           cout<<"addition = "<<num1*num2;
+           cout<<"\n";
+       }
+
+        void div()
+       {
+           cout<<"enter two number for division : ";
+           cin>>num1>>num2;
+           cout<<"addition = "<<(float)num1/num2;
+           cout<<"\n";
+       }
+};
+ int main()
+ {
+     //creation of object
+      operations op1;
+      op1.add();
+      op1.sub();
+      op1.mul();
+      op1.div();
+      return 0;
+ }
+
+
+
+//62. Operator Overloading Example.
+
+#include <iostream>
+using namespace std;
+
+class Box
+{
+   public:
+
+      double getVolume(void)
+      {
+         return length * breadth * height;
+      }
+      void setLength( double len )
+      {
+          length = len;
+      }
+
+      void setBreadth( double bre )
+      {
+          breadth = bre;
+      }
+
+      void setHeight( double hei )
+      {
+          height = hei;
+      }
+      // Overload + operator to add two Box objects.
+      Box operator+(const Box& b)
+      {
+         Box box;
+         box.length = this->length + b.length;
+         box.breadth = this->breadth + b.breadth;
+         box.height = this->height + b.height;
+         return box;
+      }
+   private:
+      double length;      // Length of a box
+      double breadth;     // Breadth of a box
+      double height;      // Height of a box
+};
+// Main function for the program
+int main( )
+{
+   Box Box1;                // Declare Box1 of type Box
+   Box Box2;                // Declare Box2 of type Box
+   Box Box3;                // Declare Box3 of type Box
+   double volume = 0.0;     // Store the volume of a box here
+
+   // box 1 specification
+   Box1.setLength(6.0);
+   Box1.setBreadth(7.0);
+   Box1.setHeight(5.0);
+
+   // box 2 specification
+   Box2.setLength(12.0);
+   Box2.setBreadth(13.0);
+   Box2.setHeight(10.0);
+
+   // volume of box 1
+   volume = Box1.getVolume();
+   cout << "Volume of Box1 : " << volume <<endl;
+
+   // volume of box 2
+   volume = Box2.getVolume();
+   cout << "Volume of Box2 : " << volume <<endl;
+
+   // Add two object as follows:
+   Box3 = Box1 + Box2;
+
+   // volume of box 3
+   volume = Box3.getVolume();
+   cout << "Volume of Box3 : " << volume <<endl;
+
+   return 0;
+}
